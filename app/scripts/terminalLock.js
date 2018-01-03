07eda8c2bb2f8e351ed75d522415aace7687f840
John A. Hopper <jahopp@gmail.com>
2018-01-03 14:15:54 -0600

    initial commit

---
+++ app/scripts/terminalLock.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.type-it').typeIt({
+     
+       speed:40,
+       autoStart: true,
+       startDelete: false,
+       breakDelay: true,
+
+
+       
+   })
+
+  
+});
